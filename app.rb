127a646f0ae87df199c5239892d5ddd1c539b427
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-10 22:58:06 +0000

    add results randomizer

--- app.rb
+++ app.rb
@@ -5,6 +5,15 @@ get("/") do
   erb(:home)
 end
 
-get("results") do
+get("/results") do
+  @num_dice = params.fetch("dice").to_i
+  @num_sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    @rolls.push(rand(1..@num_sides))
+  end
+
   erb(:results)
 end
